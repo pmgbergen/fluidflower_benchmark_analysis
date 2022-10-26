342bce1074268b4912c58e67a13851c685955d83
Jakub Both <jakub.both@uib.no>
2022-10-26 12:32:56 +0200

    update analysis script making it compatible with c1

--- c2/analysis.py
+++ c2/analysis.py
@@ -9,14 +9,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import skimage
 
-from fluidflower import BenchmarkRig
+from fluidflower import BenchmarkCO2Analysis
 
+# NOTE: Path needs update for each user - general process planned
 folder = Path("/home/jakub/images/ift/benchmark/c2")
 baseline = Path("baseline")
 
 # Define FluidFlower with first 20 baseline images
 baseline_images = list(sorted((folder / baseline).glob("*.JPG")))
-ff = BenchmarkRig(baseline_images, config="./config.json", update_setup=False)
+ff = BenchmarkCO2Analysis(baseline_images, config="./config.json", update_setup=False)
 
 # Extract concentration.
 images = list(sorted(folder.glob("*.JPG")))
@@ -32,24 +33,24 @@ for num, img in enumerate(images):
     co2 = ff.determine_co2_mask()
 
     # Determine binary mask detecting mobile CO2.
-    mobile_co2 = ff.determine_mobile_co2_mask(co2)
+    co2_gas = ff.determine_co2_gas_mask(co2)
 
     # Create image with contours on top
     contours_co2, _ = cv2.findContours(
         skimage.img_as_ubyte(co2.img), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
-    contours_mobile_co2, _ = cv2.findContours(
-        skimage.img_as_ubyte(mobile_co2.img),
+    contours_co2_gas, _ = cv2.findContours(
+        skimage.img_as_ubyte(co2_gas.img),
         cv2.RETR_TREE,
         cv2.CHAIN_APPROX_SIMPLE,
     )
-    original_img = np.copy(ff.img_with_colorchecker.img)
+    original_img = np.copy(ff.img.img)
     original_img = skimage.img_as_ubyte(original_img)
     cv2.drawContours(original_img, contours_co2, -1, (0, 255, 0), 3)
-    cv2.drawContours(original_img, contours_mobile_co2, -1, (255, 255, 0), 3)
+    cv2.drawContours(original_img, contours_co2_gas, -1, (255, 255, 0), 3)
 
     # Plot corrected image with contours
-    if False:
+    if True:
         plt.figure()
         plt.imshow(original_img)
         plt.show()
@@ -61,18 +62,18 @@ for num, img in enumerate(images):
     # Store fine scale segmentation
     segmentation = np.zeros(ff.img.img.shape[:2], dtype=int)
     segmentation[co2.img] += 1
-    segmentation[mobile_co2.img] += 1
+    segmentation[co2_gas.img] += 1
     np.save(f"segmentation/{img_id}_segmentation.npy", segmentation)
 
     # Store coarse scale segmentation
     coarse_shape = (150, 280)
     coarse_segmentation = np.zeros(coarse_shape, dtype=int)
     co2_coarse = skimage.img_as_bool(skimage.transform.resize(co2.img, coarse_shape))
-    mobile_co2_coarse = skimage.img_as_bool(
-        skimage.transform.resize(mobile_co2.img, coarse_shape)
+    co2_gas_coarse = skimage.img_as_bool(
+        skimage.transform.resize(co2_gas.img, coarse_shape)
     )
     coarse_segmentation[co2_coarse] += 1
-    coarse_segmentation[mobile_co2_coarse] += 1
+    coarse_segmentation[co2_gas_coarse] += 1
     np.save(f"segmentation/{img_id}_coarse_segmentation.npy", coarse_segmentation)
 
     print(f"Elapsed time for {img.name}: {time.time()- tic}.")
