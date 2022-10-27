29b1c57192c9567b5ddf8f87a7c4e21c4efdaf04
Jakub Both <jakub.both@uib.no>
2022-10-27 02:30:36 +0200

    add bc02 analysis - so far correction and segmentation addressed

---
+++ analysis/bc02/analyis.py
@@ -0,0 +1,30 @@
+"""
+Analysis of FluidFlower Benchmark Run BC02.
+"""
+from pathlib import Path
+
+from benchmark.standardsetups.mediumco2analysis import MediumCO2Analysis
+
+# Define the location for images of BC02 (all images in the folder)
+images_folder = Path("/home/jakub/images/ift/medium/BC02")
+images = list(sorted(images_folder.glob("*.JPG")))
+
+# Define the location of all baseline images
+baseline_folder = images_folder / Path("baseline")
+baseline = list(sorted(baseline_folder.glob("*.JPG")))
+
+# Define the location of the config file for BC02
+config = Path("./config.json")
+
+# Define FluidFlower based on a full set of basline images
+analysis = MediumCO2Analysis(
+    baseline=baseline,  # paths to baseline images
+    config=config,  # path to config file
+    update_setup=True,  # flag controlling whether aux. data needs update
+)
+
+# Perform standardized CO2 batch analysis on all images from BC02.
+analysis.batch_analysis(
+    images=images,  # paths to images to be considered
+    plot_contours=True,  # print contour lines for CO2 onto image
+)
