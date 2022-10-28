19b62262a2f6a7921d887d77f3e50d3652aec4c5
Jakub Both <jakub.both@uib.no>
2022-10-28 13:52:17 +0200

    fix wrong comments

--- analysis/c2/analysis.py
+++ analysis/c2/analysis.py
@@ -2,10 +2,9 @@
 Analysis of FluidFlower Benchmark Run C2.
 """
 from pathlib import Path
-
 from benchmark.standardsetups.benchmarkco2analysis import BenchmarkCO2Analysis
 
-# Define the location for images of C1 (all images in the folder)
+# Define the location for images of C2 (all images in the folder)
 images_folder = Path("/home/jakub/images/ift/benchmark/c2")
 images = list(sorted(images_folder.glob("*.JPG")))
 
@@ -21,9 +20,10 @@ analysis = BenchmarkCO2Analysis(
     baseline=baseline,  # paths to baseline images
     config=config,  # path to config file
     update_setup=False,  # flag controlling whether aux. data needs update
+    verbosity = False,   # print intermediate results to screen
 )
 
-# Perform standardized CO2 batch analysis on all images from C1.
+# Perform standardized CO2 batch analysis on all images from C2.
 analysis.batch_analysis(
     images=images,  # paths to images to be considered
     plot_contours=False,  # print contour lines for CO2 onto image
