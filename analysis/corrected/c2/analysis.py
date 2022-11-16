04bcff02d8ffbe2675badfd2b507b50ec70bc087
Jakub Both <jakub.both@uib.no>
2022-11-16 02:23:35 +0100

    fix c2 for test run

--- analysis/corrected/c2/analysis.py
+++ analysis/corrected/c2/analysis.py
@@ -25,6 +25,8 @@ co2_analysis.batch_analysis(
     images=images,  # paths to images to be considered
     plot_contours=False,  # print contour lines for CO2 onto image
     write_contours_to_file=True,
+    write_segmentation_to_file = True,
+    write_coarse_segmentation_to_file = True,
     # ...for more options, check the keyword arguments of BenchmarkCO2Analysis.batch_analysis.
 )
 
