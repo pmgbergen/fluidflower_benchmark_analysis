5c6c7450c47f09c53b4a9bae9bea83a0c05443d9
Jakub Both <jakub.both@uib.no>
2022-11-19 17:22:40 +0100

    include all runs

--- analysis/c1-5/run_all.sh
+++ analysis/c1-5/run_all.sh
@@ -3,9 +3,9 @@ cd c1
 python segmentation.py
 cd ../c2
 python segmentation.py
-#cd ../c3
-#python segmentation.py
-#cd ../c4
-#python segmentation.py
-#cd ../c5
-#python segmentation.py
+cd ../c3
+python segmentation.py
+cd ../c4
+python segmentation.py
+cd ../c5
+python segmentation.py
