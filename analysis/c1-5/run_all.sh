47c3966ecae3e9cff040fbe1938badc3ca635d33
Jakub Both <jakub.both@uib.no>
2022-11-19 11:04:46 +0100

    script to run all files

---
+++ analysis/c1-5/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd c1
+python segmentation.py
+cd ../c2
+python segmentation.py
+#cd ../c3
+#python segmentation.py
+#cd ../c4
+#python segmentation.py
+#cd ../c5
+#python segmentation.py
