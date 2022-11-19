81ab37a4bc280421815c3ec70635fda241ae35cd
Jakub Both <jakub.both@uib.no>
2022-11-19 02:01:02 +0100

    unify analysis of c1-5

---
+++ analysis/c1-5/c5/segmentation.py
@@ -0,0 +1,22 @@
+"""
+Analysis of FluidFlower Benchmark Run C5.
+"""
+from pathlib import Path
+import cv2
+import skimage
+
+from benchmark.standardsetups.benchmarkco2analysis import BenchmarkCO2Analysis
+from benchmark.utils.misc import read_paths_from_user_data
+
+# Read user-defined paths to images, number of baseline images, and config file
+images, baseline, config, results = read_paths_from_user_data("user_data.json")
+
+# Define FluidFlower based on a full set of basline images
+co2_analysis = BenchmarkCO2Analysis(
+    baseline=baseline,  # paths to baseline images
+    config=config,  # path to config file
+    results = results, # path to results directory
+)
+
+# Perform standardized CO2 segmentation on all images
+co2_analysis.batch_segmentation(images)
