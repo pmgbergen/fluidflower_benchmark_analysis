c424f310a06763f4da47f910a406a787cab2704b
Jakub Both <jakub.both@uib.no>
2022-11-20 21:53:39 +0100

    Version used for latest test run: Dump threshold values to file and update config

--- analysis/c1-5/c4/segmentation.py
+++ analysis/c1-5/c4/segmentation.py
@@ -4,6 +4,7 @@ Analysis of FluidFlower Benchmark Run C4.
 from pathlib import Path
 import cv2
 import skimage
+import json
 
 from benchmark.standardsetups.benchmarkco2analysis import BenchmarkCO2Analysis
 from benchmark.utils.misc import read_paths_from_user_data
@@ -20,3 +21,11 @@ co2_analysis = BenchmarkCO2Analysis(
 
 # Perform standardized CO2 segmentation on all images
 co2_analysis.batch_segmentation(images)
+
+## Print threshold cache
+out_file = open("co2_threshold.json", "w")
+json.dump(co2_analysis.co2_analysis.threshold_cache, out_file, indent=4)
+out_file.close()
+gas_out_file = open("co2_gas_threshold.json", "w")
+json.dump(co2_analysis.co2_gas_analysis.threshold_cache, gas_out_file, indent=4)
+gas_out_file.close()
