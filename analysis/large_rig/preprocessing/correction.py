e5e01aa33d1683d5f237d46904da69e7efc41428
Jakub Both <jakub.both@uib.no>
2022-11-25 11:23:13 +0100

    Make correction script work - for a hardcoded path

--- analysis/large_rig/preprocessing/correction.py
+++ analysis/large_rig/preprocessing/correction.py
@@ -9,43 +9,36 @@ import os
 
 import darsia as da
 from pathlib import Path
+import json
+
+# Define paths to data
+images = Path("/media/jakub/Elements/Jakub/benchmark/data/large_rig/tmp")
+baseline = Path("/media/jakub/Elements/Jakub/benchmark/data/large_rig/tmp/211124_time082740_DSC00067.TIF")
+
+# Define paths for storing the corrected data
+output = Path("/media/jakub/Elements/Jakub/benchmark/data/large_rig/out")
 
 # Define path to config file for correction
 config_path = Path("../config_correction.json")
-
-# Define paths to data
-folders_path = Path("H:/sets/raw_tiff2/")
-folders = os.listdir(folders_path)
-
-base_img_path = Path("H:/sets/raw_tiff2/c1/211124_time082740_DSC00067.TIF")
-
-# Define paths for storing the corrected data
-save_path = Path("H:/sets/tiff_corrected/")
+f = open(config_path, "r")
+config = json.load(f)
+f.close()
 
 # Define correction objects
-curv_correction = da.CurvatureCorrection(config=config_path)
-drift_correction = da.DriftCorrection(base=base_img_path, config=config_path)
-color_correction = da.ColorCorrection(config=config_path)
+curvature_correction = da.CurvatureCorrection(config=config["curvature"])
+drift_correction = da.DriftCorrection(base=baseline, config=config["drift"])
+color_correction = da.ColorCorrection(config=config["color"])
 
-# Perform correction for all images
-for folder in folders:
+# Correct each original image and store the corrected image to file
+for img in images.glob("*"):
 
-    # Add folder structure if required
-    images_path = folders_path + folder + "/"
-    images = os.listdir(images_path)
-    try:
-        os.mkdir(save_path + folder)
-    except:
-        None
+    # Correct
+    corrected_image = da.Image(
+        img,
+        color_correction=color_correction,
+        drift_correction=drift_correction,
+        curvature_correction=curvature_correction,
+    )
 
-    # Correct each original image and store the corrected image to file
-    for img in images:
-        new_corrected_image = da.Image(
-            images_path + img,
-            color_correction=color_correction,
-            drift_correction=drift_correction,
-            curvature_correction=curv_correction,
-            width=2.8,
-            height=1.5,
-        )
-        new_corrected_image.write(save_path + "/" + folder + "/" + img)
+    # Write to file
+    corrected_image.write(output / img.name)
