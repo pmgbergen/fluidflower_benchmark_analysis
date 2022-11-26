ea29da2c4302d1d45bbf8e20d066290cead0eb47
Jakub Both <jakub.both@uib.no>
2022-11-26 23:37:19 +0100

    update overall script

--- analysis/albus/run_all.sh
+++ analysis/albus/run_all.sh
@@ -3,8 +3,9 @@
 # Do not forget to update user_data.json files in each folder,
 # and specify the path to the images.
 
-# Preprocessing - generate geometry segmentation
+# Preprocessing - generate geometry segmentation, and determine the drift of all setups relative to AC02
 cd preprocessing
+python processing.py
 python alignment.py
 
 # Run segmentation for all experimental runs
