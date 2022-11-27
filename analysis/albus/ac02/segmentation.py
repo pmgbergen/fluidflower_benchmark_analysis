e69d0ad2f972bbbb9733a013680906c4b841fa9c
Jakub Both <jakub.both@uib.no>
2022-11-27 13:32:56 +0100

    update ac02: relax posterior

--- analysis/albus/ac02/segmentation.py
+++ analysis/albus/ac02/segmentation.py
@@ -3,7 +3,6 @@ Standard segmentation script, for Medium (albus) rig.
 """
 from benchmark.standardsetups.mediumco2analysis import MediumCO2Analysis
 from benchmark.utils.misc import read_paths_from_user_data
-import json
 
 # Read user-defined paths to images, number of baseline images, and config file
 images, baseline, config, results = read_paths_from_user_data("user_data.json")
@@ -16,6 +15,10 @@ analysis = MediumCO2Analysis(
 )
 
 #images = images[:144] + images[144::5]
+images = images[::3]
 
 # Perform standardized CO2 batch analysis on all images from BC02.
-analysis.batch_segmentation(images)
+analysis.batch_segmentation(
+    images,
+    #plot_contours=True
+)
