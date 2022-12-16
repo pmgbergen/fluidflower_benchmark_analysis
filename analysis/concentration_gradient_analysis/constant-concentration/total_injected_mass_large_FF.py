75ec47ea5bf678de603f5b1a56bb9de1e7a72282
Jakub Both <jakub.both@uib.no>
2022-12-16 21:33:50 +0100

    combine total mass computations for port 1 and 2

--- analysis/concentration_gradient_analysis/constant-concentration/total_injected_mass_large_FF.py
+++ analysis/concentration_gradient_analysis/constant-concentration/total_injected_mass_large_FF.py
@@ -3,12 +3,19 @@ Hardcoded injection profile for the large fluid flower.
 """
 
 
-def total_mass(t):
+def total_mass_co2_port1(t: float) -> float:
     """
-    Returns total injected mass (grams) as a function of time (minutes).
+    Returns total injected CO2 mass (grams) injected through port 1,
+    as a function of time (minutes).
 
     NOTE: Many numbers can be added together, but was implemented like
     this for readibility of the developer.
+
+    Args:
+        t (float): time in minutes.
+
+    Returns:
+        float: total CO2 mass injected in port 2.
     """
     if t < 4.5:
         return 1.82 * 10 / 2 * t / 1000
@@ -27,4 +34,39 @@ def total_mass(t):
         return (
             1.82 * 10 * 4.5 / 1000
             + 1.82 * 10 * (300 - 4.5) / 1000
+        )
+
+def total_mass_co2_port2(t: float) -> float:
+    """
+    Returns total injected CO2 mass (grams) injected through port 2,
+    as a function of time (minutes).
+
+    NOTE: Many numbers can be added together, but was implemented like
+    this for readibility of the developer.
+
+    Args:
+        t (float): time in minutes.
+
+    Returns:
+        float: total CO2 mass injected in port 2.
+    """
+    if t < 135:
+        return 0
+    elif t < 135 + 4.5:
+        return 1.82 * 10 / 2 * (t-135) / 1000
+    elif t < 300:
+        return (
+            1.82 * 10 / 2 * 4.5 / 1000
+            + 1.82 * 10 * (t - 135 - 4.5) / 1000
+        )
+    elif t < 300 + 4.5:
+        return (
+            1.82 * 10 / 2 * 4.5 / 1000
+            + 1.82 * 10 * (300 - 135 - 4.5) / 1000
+            + 1.82 * 10 / 2 * (t - 300) / 1000
+        )
+    else:
+        return (
+            1.82 * 10 * 4.5 / 1000
+            + 1.82 * 10 * (300 - 135 - 4.5) / 1000
         )
