137f7a43b57930bf0b7b97826006568ea7714cbb
Jakub Both <jakub.both@uib.no>
2023-02-09 23:13:35 +0100

    Use new large rig analysis object

--- analysis/large_rig/c1/segmentation.py
+++ analysis/large_rig/c1/segmentation.py
@@ -6,14 +6,14 @@ import cv2
 import skimage
 import json
 
-from benchmark.standardsetups.benchmarkco2analysis import BenchmarkCO2Analysis
+from benchmark.standardsetups.largerigco2analysis import LargeRigCO2Analysis
 from benchmark.utils.misc import read_paths_from_user_data
 
 # Read user-defined paths to images, number of baseline images, and config file
 images, baseline, config, results = read_paths_from_user_data("user_data.json")
 
 # Define FluidFlower based on a full set of basline images
-co2_analysis = BenchmarkCO2Analysis(
+co2_analysis = LargeRigCO2Analysis(
     baseline=baseline,  # paths to baseline images
     config=config,  # path to config file
     results = results, # path to results directory
