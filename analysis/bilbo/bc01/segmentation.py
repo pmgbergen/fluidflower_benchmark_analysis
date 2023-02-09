afda03bb7911fd1db8bd200b553b6f52c68536a8
Jakub Both <jakub.both@uib.no>
2023-02-09 22:19:07 +0100

    Update segmentation script

--- analysis/bilbo/bc01/segmentation.py
+++ analysis/bilbo/bc01/segmentation.py
@@ -1,19 +1,18 @@
 """
-Standard segmentation script, for Medium rig.
+Standard segmentation script, for TableTopFluidFlower rig.
 """
-from benchmark.standardsetups.mediumco2analysis import MediumCO2Analysis
+from benchmark.standardsetups.tabletopco2analysis import TableTopFluidFlowerCO2Analysis
 from benchmark.utils.misc import read_paths_from_user_data
 
 # Read user-defined paths to images, number of baseline images, and config file
 images, baseline, config, results = read_paths_from_user_data("user_data.json")
 
 # Define FluidFlower based on a full set of basline images
-analysis = MediumCO2Analysis(
+analysis = TableTopFluidFlowerCO2Analysis(
     baseline=baseline,  # paths to baseline images
     config=config,  # path to config file
     results = results, # path to results directory
 )
 
-# images = images[200:] 
-# Perform standardized CO2 batch analysis on all images from BC02.
+# Perform standardized CO2 batch analysis.
 analysis.batch_segmentation(images)
